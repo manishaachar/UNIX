f87395f827a4946ccbb09b0e12411e4c0070123d
manishaachar1320@gmail.com <manishaachar1320@gmail.com>
2022-03-07 15:36:52 +0530

    UNIX shellscript

---
+++ 7a.sh
@@ -0,0 +1,29 @@
+user=`whoami`
+set -- `who`
+for i in $*
+do
+user=$1
+if [ $user = $user ]
+then
+set -- `echo $4 |tr ":" " "`
+h=$1
+if [ $h -ge 4 ] && [ $h -lt 12 ]
+then
+echo "Good morning Mr./Mrs. $user"
+elif [ $h -ge 12 ] && [ $h -lt 16 ]
+then
+echo "Good Afternoon Mr./Mrs. $user"
+elif [ $h -ge 16 ] && [ $h -lt 19 ]
+then
+echo "Good Evening Mr./Mrs. $user"
+else
+echo "Good Night Mr./Mrs. $user"
+fi
+exit
+else
+shift 5
+fi
+done
+
+
+
